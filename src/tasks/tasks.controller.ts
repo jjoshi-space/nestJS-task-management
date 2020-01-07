53dc8d4550eb33737a7ab35a37df73a21491189f
Jay Joshi <jay.joshi@coditas.com>
2020-01-07 16:54:31 +0530

    Created task management application using NestJS

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,62 @@
+import { Controller,Get,Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, Logger } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
+import { create } from 'domain';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+    private logger = new Logger('TasksController');
+    constructor(private tasksService: TasksService){}
+
+    @Get()
+    getTasks(
+        @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+        @GetUser() user: User,
+        ) {
+        this.logger.verbose(`User ${user.username} retrieving all tasks. Filters ${JSON.stringify(filterDto)} `);
+        return this.tasksService.getTasks(filterDto,user);
+    }
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createTask(
+        @Body() createTaskDto: CreateTaskDto,
+        @GetUser() user: User,
+        ): Promise<Task> {
+        this.logger.verbose(`User ${user.username} creating a new task. Data ${JSON.stringify(createTaskDto)}`);          
+        return this.tasksService.createTask(createTaskDto,user);
+    }
+
+    @Get('/:id')
+    getTaskById(
+        @Param('id',ParseIntPipe) id:number,
+        @GetUser() user: User,
+        ): Promise<Task>{
+        return this.tasksService.getTaskById(id,user);
+    }
+
+    @Delete('/:id')
+    deleteTaskById(
+        @Param('id') id:number,
+        @GetUser() user: User,
+        ): Promise<void> {
+        return this.tasksService.deleteTaskById(id,user);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id:number,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User
+        ): Promise<Task>{
+        return this.tasksService.updateTaskStatus(id,status,user);
+    }
+}
